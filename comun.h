f4c87488d2f149c70c9e6528885d644e67e44db5
THEKLEVER <kleverdesousa@gmail.com>
2013-12-07 16:57:48 +0100

    struc

---
+++ comun.h
@@ -0,0 +1,15 @@
+/*-----------------------------------*/
+/* Structures Composants et Produits */
+/*-----------------------------------*/
+
+typedef struct Composant{
+    int num;		
+}Composant;
+
+typedef struct Produit{
+    int ops[10];
+    int typeCompo;
+    int nbComp;
+    char etat[10];
+}Produit;
+
---
+++ robot.h
@@ -0,0 +1,10 @@
+/*-----------------*/
+/* Structure Robot */
+/*-----------------*/
+
+typedef struct Robot{
+    int ops[10];
+    int stockComp[4];
+    char mode[10];
+}Robot;
+
